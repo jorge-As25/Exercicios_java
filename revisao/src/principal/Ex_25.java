03047164492728ad2b388d38072537cc58efcbc1
JORGE ARAUJO DA SILVA <JORGE_4878@uirapuru.br>
2025-04-03 18:02:20 -0300

    exercicios de java

---
+++ revisao/src/principal/Ex_25.java
@@ -0,0 +1,18 @@
+
+package principal;
+
+import static java.lang.Double.parseDouble;
+import java.util.Scanner;
+
+
+public class Ex_25 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        
+        double velocidade;
+        
+        System.out.println("Digite a velocidade que vc estava dirigindo");
+        velocidade = parseDouble(x.next());
+
+    } 
+}
